d8e3ee87cf1713b1a33bbc0b90187a7020fba9e4
adnanalvee <adnanalvee>
2023-10-07 19:59:01 +0000

    test

---
+++ draft/cust_jd/fs_draft.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+import paaw1.paaw2
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+import sys
+sys.path.append("/Users/adnan.alvee@databricks.com/cust:jd/paaw1/paaw2")
+
+
+# COMMAND ----------
+
+from paaw2 import *
